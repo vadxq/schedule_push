b2cf7bf17008832a95837006f27c185581ce7c64
vadxq <dongxianlin@vadxq.com>
2017-10-05 21:01:51 +0800

    finish config.js index.js

---
+++ config.js
@@ -0,0 +1,99 @@
+exports.detailMail = {
+  subject: '勺薠呀，勺薠呀',
+  text: '勺薠呀，快上课了哟～',
+  htmlName: '<b>勺薠呀，快上课了哟～</b><br/><br/>',
+  htmlLeftname: '---专为你定制的课表邮件提醒<br/>---来自你帅气的显林叔'
+}
+
+exports.anotherMail = {
+  keyword: 'wanan',
+  text: '勺薠呀,晚安',
+  html: '<b>勺薠呀，快去睡哟～</b><br/>不然不给你讲故事了！<br/><br/>---专为你定制的晚安提醒<br/>---来自你帅气的显林叔'
+}
+
+exports.classes = [{
+  // 1
+  time: '10 30 07 * * 1',
+  course: '人体解剖学1',
+  courseTime: '07:50-12:10',
+  addr: '人体解剖实验室'
+},{
+  // 2
+  time: '10 30 13 * * 1',
+  course: '应用软件类实验',
+  courseTime: '13:50-15:25',
+  addr: '机房'
+},{
+  // 3
+  time: '10 30 07 * * 2',
+  course: '高等数学',
+  courseTime: '07:50-09:25',
+  addr: '2-408'
+},{
+  // 4
+  time: '10 30 09 * * 2',
+  course: '思想道德修养与法律基础',
+  courseTime: '09:45-11:20',
+  addr: '2-408'
+},{
+  // 5
+  time: '10 30 13 * * 2',
+  course: '人体解剖学1',
+  courseTime: '13:50-18:10',
+  addr: '人体解剖实验室'
+},{
+  // 6
+  time: '10 20 09 * * 3',
+  course: '大学生心理健康指导',
+  courseTime: '09:45-11:20',
+  addr: '2-408'
+},{
+  // 7
+  time: '10 30 13 * * 3',
+  course: '体育',
+  courseTime: '13:50-15:25',
+  addr: '运动场'
+},{
+  // 8
+  time: '10 30 07 * * 4',
+  course: '应用软件类',
+  courseTime: '07:50-09:25',
+  addr: '机房'
+},{
+  // 9
+  time: '10 30 09 * * 4',
+  course: '高等数学',
+  courseTime: '09:45-11:25',
+  addr: '2-408'
+},{
+  // 10
+  time: '10 30 13 * * 4',
+  course: '细胞生物学',
+  courseTime: '13:50-16:30',
+  addr: '2-408'
+},{
+  //11
+  time: '10 30 07 * * 5',
+  course: '人体解剖学1',
+  courseTime: '07:50-12:10',
+  addr: '人体解剖实验室'
+},{
+  time: '10 30 13 * * 5',
+  course: '外文',
+  courseTime: '13:50-16:30',
+  addr: '语音室'
+},{
+  time: '30 55 23 * * *',
+  courseTime: 'wanan'
+}
+// test
+// {
+//   time: '57 59 20 * * 4',
+//   course: '外文',
+//   courseTime: '13:50-16:30',
+//   addr: '语音室'
+// },{
+//   time: '57 59 20 * * *',
+//   courseTime: 'wanan'
+// }
+]
---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict'
+const schedule = require('node-schedule'),
+  nodemailer = require('nodemailer'),
+  fs = require('fs'),
+  config = require('./config'),
+  auth = require('./auth')
+
+// logs
+const logger = (msg, info) => {
+  let time = (new Date).toLocaleString()
+  let stackInfoStr = stackInfo()
+  msg = info + ' ' + time + ` ${stackInfoStr.file}:${stackInfoStr.line} ` + msg + '\n'
+  fs.writeFile('./logs.log', msg, { 'flag': 'a' }, (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Saved.')
+  })
+}
+
+const scheduleTask = (time, data) => {
+  schedule.scheduleJob(time, () => {
+    console.log(data)
+    logger(time, 'WARN')
+    logger(data, 'data')
+    mails(data)
+  })
+}
+
+for (const {time, course, courseTime, addr} of config.classes) {
+  console.log(time)
+  scheduleTask(time, {courseTime: courseTime, course: course, addr: addr})
+}
+
+//邮件服务
+const mails = (data) => {
+  nodemailer.createTestAccount((err, account) => {
+    let transporter = nodemailer.createTransport({
+      host: auth.port.host,
+      port: auth.port.port,
+      secure: auth.port.secure,
+      auth: auth.auth
+    })
+
+    let mailOptions = {
+      from: auth.mymail, 
+      to: auth.tomail, 
+      subject: config.detailMail.subject,
+      text: config.detailMail.text,
+      html: config.detailMail.htmlName + data.courseTime + '的' + data.course + '课即将在' + data.addr + '开始，准备一下啦，别迟到了哟～<br/><br/>' + config.detailMail.htmlLeftname
+    }
+    
+    if (data.courseTime === config.anotherMail.keyword) {
+      mailOptions.text = config.anotherMail.text,
+      mailOptions.html = config.anotherMail.html
+    }
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return logger(error, 'error')
+      }
+      console.log('Message sent: %s', info.messageId)
+      logger('Message sent: %s' + info.messageId, 'success')
+    })
+  })
+}
+
+/**
+* 追踪日志输出文件名,方法名,行号等信息
+* @returns Object
+*/
+const stackInfo = () => {
+  let path = require('path')
+  let stackReg = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/i
+  let stackReg2 = /at\s+()(.*):(\d*):(\d*)/i
+  let stacklist = (new Error()).stack.split('\n').slice(3)
+  let s = stacklist[0]
+  let sp = stackReg.exec(s) || stackReg2.exec(s)
+  let data = {}
+  if (sp && sp.length === 5) {
+    data.method = sp[1]
+    data.path = sp[2]
+    data.line = sp[3]
+    data.pos = sp[4]
+    data.file = path.basename(data.path)
+  }
+  return data
+}
