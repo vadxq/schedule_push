a5c2d4a4550e84528b46b237079a095c408098ac
vadxq <dongxianlin@vadxq.com>
2017-10-05 22:03:13 +0800

    delete name

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 exports.detailMail = {
-  subject: '勺薠呀，勺薠呀',
-  text: '勺薠呀，快上课了哟～',
-  htmlName: '<b>勺薠呀，快上课了哟～</b><br/><br/>',
+  subject: '某某呀，某某呀',
+  text: '某某呀，快上课了哟～',
+  htmlName: '<b>某某呀，快上课了哟～</b><br/><br/>',
   htmlLeftname: '---专为你定制的课表邮件提醒<br/>---来自你帅气的显林叔'
 }
 
 exports.anotherMail = {
   keyword: 'wanan',
-  subject: '勺薠呀，勺薠呀',
-  text: '勺薠呀,晚安',
-  html: '<b>勺薠呀，快去睡哟～</b><br/>不然不给你讲故事了！<br/><br/>---专为你定制的晚安提醒<br/>---来自你帅气的显林叔'
+  subject: '某某呀，某某呀',
+  text: '某某呀,晚安',
+  html: '<b>某某呀，快去睡哟～</b><br/>不然不给你讲故事了！<br/><br/>---专为你定制的晚安提醒<br/>---来自你帅气的显林叔'
 }
 
 exports.classes = [{
