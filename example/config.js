5ccbcd4f127cd317dd564a5f083e43dcf0725289
vadxq <dongxianlin@vadxq.com>
2017-10-23 16:23:56 +0800

    add dev and example

---
+++ example/config.js
@@ -0,0 +1,107 @@
+exports.detailMail = {
+  subject: '勺煩呀，勺煩呀',
+  text: '勺煩呀，快上课了哟～',
+  htmlName: '<b>勺煩呀，快上课了哟～</b><br/><br/>',
+  htmlLeftname: '---专为你定制的课表邮件提醒<br/>---来自你帅气的显林叔'
+}
+
+exports.anotherMail = {
+  keyword: 'wanan',
+  subject: '勺煩呀，勺煩呀',
+  text: '勺煩呀,晚安',
+  html: '<b>勺煩呀，快去睡哟～</b><br/>不然不给你讲故事了！<br/><br/>---专为你定制的晚安提醒<br/>---来自你帅气的显林叔'
+}
+
+exports.classes = [{
+  // 1-1
+  time: '10 30 07 * * 1',
+  course: '人体解剖学1',
+  courseTime: '07:50-12:10',
+  addr: '人体解剖实验室'
+},{
+  // 1-2
+  time: '10 30 13 * * 1',
+  course: '应用软件类实验',
+  courseTime: '13:50-15:25',
+  addr: '机房'
+},{
+  // 1-3
+  time: '10 15 15 * * 1',
+  course: '形式与政策',
+  courseTime: '15:45-17:20',
+  addr: '2-408'
+},{
+  // 2-1
+  time: '10 30 07 * * 2',
+  course: '高等数学',
+  courseTime: '07:50-09:25',
+  addr: '2-408'
+},{
+  // 2-2
+  time: '10 30 09 * * 2',
+  course: '思想道德修养与法律基础',
+  courseTime: '09:45-11:20',
+  addr: '2-408'
+},{
+  // 2-3
+  time: '10 30 13 * * 2',
+  course: '人体解剖学1',
+  courseTime: '13:50-18:10',
+  addr: '人体解剖实验室'
+},{
+  // 3-1
+  time: '10 20 09 * * 3',
+  course: '大学生心理健康指导',
+  courseTime: '09:45-11:20',
+  addr: '2-408'
+},{
+  // 3-2
+  time: '10 30 13 * * 3',
+  course: '体育',
+  courseTime: '13:50-15:25',
+  addr: '运动场'
+},{
+  // 4-1
+  time: '10 30 07 * * 4',
+  course: '应用软件类',
+  courseTime: '07:50-09:25',
+  addr: '机房'
+},{
+  // 4-2
+  time: '10 30 09 * * 4',
+  course: '高等数学',
+  courseTime: '09:45-11:25',
+  addr: '2-408'
+},{
+  // 4-3
+  time: '10 30 13 * * 4',
+  course: '细胞生物学',
+  courseTime: '13:50-16:30',
+  addr: '2-408'
+},{
+  // 5-1
+  time: '10 30 07 * * 5',
+  course: '人体解剖学1',
+  courseTime: '07:50-12:10',
+  addr: '人体解剖实验室'
+},{
+  // 5-2
+  time: '10 30 13 * * 5',
+  course: '外文',
+  courseTime: '13:50-16:30',
+  addr: '语音室'
+},{
+  time: '30 30 23 * * *',
+  courseTime: 'wanan'
+}
+// test
+// {
+//   time: '57 59 20 * * 4',
+//   course: '外文',
+//   courseTime: '13:50-16:30',
+//   addr: '语音室'
+// },{
+//   time: '57 59 20 * * *',
+//   courseTime: 'wanan'
+// }
+]
